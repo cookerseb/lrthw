d084e1c86f6c5668becbd92255cdf5daa5763576
cookerseb <esolutionsweb@gmail.com>
2016-01-06 16:51:14 +0100

    Fini les exercices 1 à 9

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+name = 'Sebastien C.'
+age = 39 # not a lie in 2015
+height = 76 # kilograms
+weight = 176 # centimeters
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Dark'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
---
+++ ex6.rb
@@ -0,0 +1,34 @@
+# set 10 to types_of_people variable
+types_of_people = 10
+
+# create a string with text and types_of_people variable in it
+x = "There are #{types_of_people} types of people."
+# create two string text
+binary = "binary"
+do_not = "don't"
+# create the y string with both lastest variables in it
+y = "Those who know #{binary} and those who #{do_not}."
+
+# write x and y strings
+puts x
+puts y
+
+# write two other lines with x and y string in puts
+puts "I said: #{x}."
+# write simple quote for y string 
+puts "I also said: '#{y}'."
+
+# set val false for hilarious var
+hilarious = false
+# add hialious var in joke_evaluation string
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# write joke_evaluation string and his hilarious var
+puts joke_evaluation
+
+# set two other string
+w = "This is the left side of..."
+e = "a string with a right side."
+
+# write and combine latests 
+puts w + e
